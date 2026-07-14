8cca3ddc9f41c22ee4130798941d291ed865e479
namanamberkar <namanamberkar@gmail.com>
2026-07-14 16:48:31 +0530

    Update GAS_URL and FALLBACK_WHATSAPP values

--- config.js
+++ config.js
@@ -8,9 +8,9 @@
 window.SWARNA_CONFIG = {
 
   // Paste your Google Apps Script Web App URL here:
-  GAS_URL: "",   // e.g. "https://script.google.com/macros/s/AKfycbz.../exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbynsfjRSYP9fzNBO13kFIAyB_08sqkUrkO4gSVpeVG026yn7rg_1y1CvixlZnHvJZ1K/exec",   // e.g. "https://script.google.com/macros/s/AKfycbz.../exec"
 
   // Fallback WhatsApp number (used ONLY if GAS is not connected).
-  FALLBACK_WHATSAPP: "919876543210"
+  FALLBACK_WHATSAPP: "917892174048"
 
 };
